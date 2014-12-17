17d8aaec58c527f25d8e16bf524fe32952dc8e31
jordanwilcken@github.com <jordanwilcken@gmail.com>
2014-12-17 11:02:46 -0700

    Initial commit. Login/Logout working.

---
+++ BirthdayAPI/App_Start/WebAPIConfig.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace BirthdayAPI
+{
+	public static class WebAPIConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			#region Routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "ControllerApi",
+				routeTemplate: "{controller}/{action}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+
+			);
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+			#endregion WebAPI Routes
+		}
+	}
+}
---
+++ BirthdayAPI/Controllers/AuthController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace BirthdayAPI.Controllers
+{
+	public class AuthController : ApiController
+	{
+		public bool Login(Thing id)
+		{
+			if (!Membership.ValidateUser(id.prop1, id.prop2))
+			{
+				return false;
+			}
+			else
+			{
+				FormsAuthentication.SetAuthCookie(id.prop1, false);
+				return true;
+			}
+		}
+
+		public void Logout()
+		{
+			FormsAuthentication.SignOut();
+		}
+
+		//below here is boiler plate
+
+		// GET api/<controller>
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<controller>/5
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<controller>
+		public void Post([FromBody]string value)
+		{
+		}
+
+		// PUT api/<controller>/5
+		public void Put(int id, [FromBody]string value)
+		{
+		}
+
+		// DELETE api/<controller>/5
+		public void Delete(int id)
+		{
+		}
+	}
+
+	public class Thing
+	{
+		public string prop1 { get; set; }
+		public string prop2 { get; set; }
+	}
+}
